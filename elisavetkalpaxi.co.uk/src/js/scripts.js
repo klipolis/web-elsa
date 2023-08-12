cf451d5cb272229ed6afc71d3e937a62b6368a6f
Karoly Czifra <k.lihelp@gmail.com>
2023-08-12 20:16:48 +0100

    Refactor(template): Improve responsiveness of thumbnail images

--- elisavetkalpaxi.co.uk/src/js/scripts.js
+++ elisavetkalpaxi.co.uk/src/js/scripts.js
@@ -2,22 +2,16 @@
 // Scripts
 // 
 
-
-// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
-// import "../../node_modules/popper.js/dist/popper.min.js";
-
 // import "../../node_modules/bootstrap/js/dist/util.js";
 // import "../../node_modules/bootstrap/js/dist/carousel.js";
-// import "../../node_modules/bootstrap/js/dist/util.js";
-
 
 
 // DOMContentLoaded
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate SimpleLightbox plugin for portfolio items
+    // Activate modal
     new SimpleLightbox({
-        elements: '.gallery a.thumbnail'
+        elements: 'a.thumbnail'
     });
 
 });
